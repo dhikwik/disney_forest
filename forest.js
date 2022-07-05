293cf4b07b7a84f7e91b97a236bef1707dad150d
unknown <19eucs030@skcet.ac.in>
2022-07-05 10:28:48 +0530

    disney

---
+++ forest.js
@@ -0,0 +1,13 @@
+function disney(opt){
+    if(opt=='elsa'){
+        document.getElementById('fro').style.visibility="visible";
+        document.getElementById('bea').style.visibility="hidden"
+
+    }else{
+        document.getElementById('fro').style.visibility="hidden";
+        document.getElementById('bea').style.visibility="visible";
+
+
+
+    }
+}
